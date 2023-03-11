537c9fda7870ead58c62d2a1bc2118576d9d29e6
1gnaci0 <otrosnombresfrancisco@gmail.com>
2023-03-10 21:06:05 -0300

    CODIGO PROLIJO!

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -15,77 +15,65 @@ public class Ejercicio11 {
 
     public static void main(String[] args) {
 
+    
+
         Scanner leer = new Scanner(System.in);
 
-        
+        System.out.println("Ingrese dos numeros:");
+        double num;
+        double num2;
+        boolean log = false;
+        num = leer.nextInt();
+        num2 = leer.nextInt();
+        do {
+            System.out.println("MENU\n"
+                    + "1. Sumar\n"
+                    + "2. Restar\n"
+                    + "3. Multiplicar\n"
+                    + "4. Dividir\n"
+                    + "5. Salir\n"
+                    + "Elija opción:");
 
-        System.out.println("Ingresa 2 numeros positivos:");
+            int op = leer.nextInt();
 
-        double num1 = leer.nextDouble();
-        double num2 = leer.nextDouble();
-        
-        System.out.println("MENU\n"
-                + "1. Sumar\n"
-                + "2. Restar\n"
-                + "3. Multiplicar\n"
-                + "4. Dividir\n"
-                + "5. Salir\n"
-                + "Elija opción:");
+            switch (op) {
 
-        int op = leer.nextInt();
-
-        while (op == 5) {
-            Scanner leer2 = new Scanner(System.in);
-            System.out.println("Esta seguro que desea salir?: S/N");
+                case 1:
+                    double suma = num + num2;
+                    System.out.println(suma);
+                    log = true;
+                    break;
+                case 2:
+                    double rest = num - num2;
+                    System.out.println(rest);
+                    log = true;
+                    break;
+                case 3:
+                    double mult = num * num2;
+                    System.out.println(mult);
+                    log = true;
+                    break;
+                case 4:
+                    double div = num / num2;
+                    System.out.println(div);
+                    log = true;
+                    break;
+               }
             
-            String op2;
-            op2 = leer2.nextLine();
-
-            if (op2.equals("S")) {
-                System.out.println("adios");
-                break;
-            } 
-            else {
-
-                System.out.println("MENU\n"
-                        + "1. Sumar\n"
-                        + "2. Restar\n"
-                        + "3. Multiplicar\n"
-                        + "4. Dividir\n"
-                        + "5. Salir\n"
-                        + "Elija opción:");
-
-                op = leer.nextInt();
+                if(op == 5){
+                    Scanner leer2 = new Scanner(System.in);
+                    System.out.println("Desea salir? S/N:");
                         
-            }
+                    String op2 = leer2.nextLine(); // char op2 = leer.next...();
 
-        }
+                    if (op2.equals("S")) {
+                        log = true;
+                    }
+                }  
+        }while(log == false);
 
-        if (op != 5) {
-            double tot = t(num1, num2, op);
-
-            System.out.println("el total es " + tot);
-        }
-
-    }
-
-    public static double t(double num1, double num2, int op) {
-
-        double t = 0;
-
-        if (op == 1) {
-
-            t = num1 + num2;
-
-        } else if ((op == 2)) {
-            t = num1 - num2;
-        } else if ((op == 3)) {
-            t = num1 * num2;
-        } else if ((op == 4)) {
-            t = num1 / num2;
-        }
-        return t;
-
-    }
-
+        
+    
+   }
 }
+
