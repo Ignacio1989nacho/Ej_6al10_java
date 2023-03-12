e6c72923f75ab05352f6cfbf154db06fffd0aaf0
1gnaci0 <otrosnombresfrancisco@gmail.com>
2023-03-12 20:42:48 -0300

    agrego public statci final, fuera del main y modifico el bucle

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -1,37 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package java_ejercicios_6al17;
+
 import java.util.Scanner;
-/**
- *
- * @author Usuario
- */
+
 public class Ejercicio12 {
-    public static void main(String[] args){
-    int cont = 0;
-    Scanner leer = new Scanner(System.in);
-    
-    do{
-    System.out.println("Escribe una palabra");
-    String palabra = leer.nextLine();
-    
-    
-    
-    
-       if ((palabra.substring(palabra.length()-1,palabra.length()).equals("O")) && (palabra.substring(0,1).equals("X")) && palabra.length() < 5) {
-       
-           System.out.println("FORMATO CORRECTO");
-           cont = 1;
-       } 
-       else{
-           System.out.println("formato incorrecto");
-       }
-    
-    }while(cont != 1);
-    
+
+    public static Scanner leer = new Scanner(System.in);
+    public static final String FIN = "&&&&&";
+
+    public static void main(String[] args) {
+        int cont = 0;
+        int cont2 = 0;
+        System.out.println("Escribe una palabra");
+        String palabra = leer.nextLine();
+
+        while (!palabra.equals(FIN)) {
+
+            if ((palabra.substring(palabra.length() - 1, palabra.length()).equals("O")) && (palabra.substring(0, 1).equals("X")) && palabra.length() < 5) {
+
+                cont++;
+
+            } else {
+
+                cont2++;
+            }
+            System.out.println("Escribe una palabra");
+            palabra = leer.nextLine();
+        }
+        System.out.println("correctas:" + cont);
+        System.out.println("incorrectas:" + cont2);
+
     }
-    
+
 }
